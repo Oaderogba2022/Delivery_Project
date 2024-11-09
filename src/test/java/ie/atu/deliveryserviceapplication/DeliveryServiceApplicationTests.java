08b2563df28bd8e528e4eb777e0e43486a4a917d
Olamide Aderogba <G00401971@atu.ie>
2024-11-09 09:48:22 +0000

    Created DeliveryServiceApplication project for the delivery's and created Delivery class

---
+++ src/test/java/ie/atu/deliveryserviceapplication/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.deliveryserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
